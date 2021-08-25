939a9a48212e60c15b97810b2d4f174dbbf42118
giancarvalho <giandcarvalho@gmail.com>
2021-08-25 13:04:34 -0300

    Added function to select and deselect items

---
+++ src/components/data.js
@@ -0,0 +1,76 @@
+const CATEGORIES = [
+  {
+    category: "food",
+    description: "Primeiro, seu prato",
+    items: [
+      {
+        name: "Hambúrguer",
+        description: "Seu hambúrguer tradicional",
+        price: 16.9,
+        image: "hamburguer.jpg",
+      },
+      {
+        name: "Hambúrguer Vegano",
+        description: "Feito a base de grão de bico",
+        price: 25.9,
+        image: "vegano.jpg",
+      },
+      {
+        name: "X-Tudo Gourmet",
+        description: "Xtudo, só que 3x mais caro",
+        price: 39.9,
+        image: "xtudo.jpg",
+      },
+    ],
+  },
+  {
+    category: "drink",
+    description: "Agora, sua bebida",
+    items: [
+      {
+        name: "Coca Quente",
+        description: "Foto meramente ilustrativa",
+        price: 4.9,
+        image: "coca.jpg",
+      },
+      {
+        name: "Coca Gelada",
+        description: "",
+        price: 6.9,
+        image: "coca.jpg",
+      },
+      {
+        name: "Caldo de Cana",
+        description: "Atenção, contém cana",
+        price: 6.0,
+        image: "cana.jpg",
+      },
+    ],
+  },
+  {
+    category: "dessert",
+    description: "Por fim, sua sobremesa",
+    items: [
+      {
+        name: "Brigadeiro",
+        description: "",
+        price: 2.9,
+        image: "brigadeiro.jpg",
+      },
+      {
+        name: "Cenoura",
+        description: "Inclui cobertura de chocolate",
+        price: 3.9,
+        image: "cenoura.png",
+      },
+      {
+        name: "Sorvete de Chocolate",
+        description: "Ou qual sabor estiver disponível",
+        price: 8.0,
+        image: "sorvete.jpg",
+      },
+    ],
+  },
+];
+
+export default CATEGORIES;
---
+++ src/scripts/selectitem.js
@@ -0,0 +1,21 @@
+let ORDER = { food: [], drink: [], dessert: [] };
+
+function isNewItem(category, name) {
+  return !category.some((object) => object.item === name);
+}
+
+function removeItem(category, name) {
+  ORDER[category] = ORDER[category].filter((object) => object.item !== name);
+}
+
+export default function SelectItem(name, price, category) {
+  console.log(name + " " + price + " " + category);
+
+  if (isNewItem(ORDER[category], name)) {
+    ORDER[category].push({ item: name, price });
+  } else {
+    removeItem(category, name);
+  }
+
+  console.log(ORDER);
+}
